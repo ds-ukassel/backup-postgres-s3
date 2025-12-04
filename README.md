b7a1bbecab8111b3b237da64602c38db377d101d
LeStegii <50475262+LeStegii@users.noreply.github.com>
2025-12-04 10:38:15 +0100

    feat: Add more config options (#6)
    * feat: Make flags configurable
    
    * feat: Dump whole db if tables aren't specified
    
    * feat: Remove 'full' from file name
    
    * feat: Remove negation for dumping ownership and privilege information

--- README.md
+++ README.md
@@ -9,13 +9,15 @@ POSTGRES_HOST='localhost'
 POSTGRES_USER='user'
 POSTGRES_PASSWORD='password'
 POSTGRES_DATABASE='mydb'
-POSTGRES_TABLES='mydb.table1 mydb.table2'
 MINIO_ENDPOINT='http://localhost:9000'
 MINIO_ACCESS_KEY='minioadmin'
 MINIO_SECRET_KEY='minioadmin'
 
 # Optional variables
 POSTGRES_PORT=5432
+POSTGRES_TABLES='table1 table2'
+POSTGRES_DUMP_OWNERS=false
+POSTGRES_DUMP_PRIVILEGES=false
 MINIO_BUCKET='postgres-backups'
 MINIO_PATH='postgres-backups'
 RETENTION_PERIOD='7d'
@@ -31,7 +33,12 @@ It will also remove backups older than the specified number of days.
 To disable this feature, leave `RETENTION_PERIOD` empty.
 
 `POSTGRES_TABLES` can be used to specify the tables to back up, separated by spaces.
+If no tables are specified, the entire database will be backed up.
 
 Backups will be stored under the specified `MINIO_PATH` in the bucket `MINIO_BUCKET`, with filenames in the format `<database>_<table>_YYYYMMDD_HHMMSS.sql.gz`.
 
 When setting `DISCORD_WEBHOOK_URL`, a notification will be sent to the specified Discord webhook when the backup fails.
+
+Setting `POSTGRES_DUMP_OWNERS` and `POSTGRES_DUMP_PRIVILEGES` to false will add the `--no-privileges` and `--no-owner` flags to the pg_dump command.
+Set them to true to include privileges and ownership information in the backup.
+Per default, privileges and ownership information is excluded from the backup.
--- scripts/backup-postgresql.sh
+++ scripts/backup-postgresql.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 # Required environment variables
 : "${POSTGRES_HOST:?Missing POSTGRES_HOST}"
 : "${POSTGRES_USER:?Missing POSTGRES_USER}"
 : "${POSTGRES_PASSWORD:?Missing POSTGRES_PASSWORD}"
 : "${POSTGRES_DATABASE:?Missing POSTGRES_DATABASE}"
-: "${POSTGRES_TABLES:?Missing POSTGRES_TABLES (space-separated list of tables)}"
 : "${MINIO_ENDPOINT:?Missing MINIO_ENDPOINT}"
 : "${MINIO_ACCESS_KEY:?Missing MINIO_ACCESS_KEY}"
 : "${MINIO_SECRET_KEY:?Missing MINIO_SECRET_KEY}"
 
 # Optional variables with defaults
 POSTGRES_PORT="${POSTGRES_PORT:-5432}"
+POSTGRES_DUMP_OWNERS="${POSTGRES_DUMP_OWNERS:-false}"
+POSTGRES_DUMP_PRIVILEGES="${POSTGRES_DUMP_PRIVILEGES:-false}"
+POSTGRES_TABLES="${POSTGRES_TABLES:-}"
 MINIO_BUCKET="${MINIO_BUCKET:-postgres-backups}"
 MINIO_PATH="${MINIO_PATH-postgres-backups}"
 RETENTION_PERIOD="${RETENTION_PERIOD:-}"
 MINIO_COMMAND="${MINIO_COMMAND:-mc}"
 DISCORD_WEBHOOK_URL="${DISCORD_WEBHOOK_URL:-}"
+
+
+PGDUMP_NO_OWNER=""
+PGDUMP_NO_PRIVILEGES=""
+[ "$POSTGRES_DUMP_OWNERS" = "false" ] && PGDUMP_NO_OWNER="--no-owner"
+[ "$POSTGRES_DUMP_PRIVILEGES" = "false" ] && PGDUMP_NO_PRIVILEGES="--no-privileges"
 
 NOW=$(date +%Y%m%d_%H%M%S)
 
@@ -37,21 +45,36 @@ echo "[postgres-backup] Starting backup at $NOW..."
 $MINIO_COMMAND alias set storage "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 $MINIO_COMMAND mb -p "storage/$MINIO_BUCKET"
 
-for TABLE in $POSTGRES_TABLES; do
-  [ -z "$TABLE" ] && continue # Skip empty table names
+if [ -z "$POSTGRES_TABLES" ]; then
+  BACKUP_FILE="${POSTGRES_DATABASE}_${NOW}.sql.gz"
 
-  BACKUP_FILE="${POSTGRES_DATABASE}_${TABLE}_${NOW}.sql.gz"
-
-  echo "[postgres-backup] Dumping '$POSTGRES_DATABASE.$TABLE' and uploading..."
+  echo "[postgres-backup] Dumping full database '$POSTGRES_DATABASE' and uploading..."
 
   # https://www.postgresql.org/docs/current/app-pgdump.html
   pg_dump \
     --host="$POSTGRES_HOST" --port="$POSTGRES_PORT" --username="$POSTGRES_USER" \
-    --no-owner --no-privileges \
-    --table="$TABLE" "$POSTGRES_DATABASE" \
+    $PGDUMP_NO_OWNER $PGDUMP_NO_PRIVILEGES \
+    "$POSTGRES_DATABASE" \
   | gzip -c \
   | $MINIO_COMMAND pipe "storage/$MINIO_BUCKET/$MINIO_PATH/$BACKUP_FILE"
-done
+
+else
+  for TABLE in $POSTGRES_TABLES; do
+    [ -z "$TABLE" ] && continue # Skip empty table names
+
+    BACKUP_FILE="${POSTGRES_DATABASE}_${TABLE}_${NOW}.sql.gz"
+
+    echo "[postgres-backup] Dumping '$POSTGRES_DATABASE.$TABLE' and uploading..."
+
+    # https://www.postgresql.org/docs/current/app-pgdump.html
+    pg_dump \
+      --host="$POSTGRES_HOST" --port="$POSTGRES_PORT" --username="$POSTGRES_USER" \
+      $PGDUMP_NO_OWNER $PGDUMP_NO_PRIVILEGES \
+      --table="$TABLE" "$POSTGRES_DATABASE" \
+    | gzip -c \
+    | $MINIO_COMMAND pipe "storage/$MINIO_BUCKET/$MINIO_PATH/$BACKUP_FILE"
+  done
+fi
 
 if [ -n "$RETENTION_PERIOD" ]; then
   echo "[postgres-backup] Deleting backups older than $RETENTION_PERIOD from MinIO..."
